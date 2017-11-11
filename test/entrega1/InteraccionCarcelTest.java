7753eb3da0000c56c174a750b2b4c2c73db12b54
Mateo <mateoicalvo@users.noreply.github.com>
2017-11-11 05:55:18 -0300

    agregados dos test de carcel

--- test/entrega1/InteraccionCarcelTest.java
+++ test/entrega1/InteraccionCarcelTest.java
@@ -29,11 +29,35 @@ public class InteraccionCarcelTest {
 	@Test
 	public void testJugadorEncarceladoPuedePagarFianzaYAlPagarlaPuedeMoverse() {
 		
+		Jugador juan = new JugadorConcreto("Juan");
+		Jugador pepe = new JugadorConcreto("Pepe");
+		Jugador luis = new JugadorConcreto("Luis");
+		Turno turno = new Turno(juan, pepe, luis);		
+		Casilla carcel = new Carcel();
+		
+		carcel.afectar(pepe);
+		carcel.pagarFianza(pepe);
+		
+		Jugador jugadorSiguiente = turno.cambiarTurno();
+		Assert.assertEquals(jugadorSiguiente, pepe);
+		
 	}
 	
 	@Test
 	public void testJugadorEncarceladoNoPuedePagarFianzaPorFaltaDeCapitalEntoncesNoPuedeMoverse() {
-				
+		
+		Jugador juan = new JugadorConcreto("Juan");
+		Jugador pepe = new JugadorConcreto("Pepe");
+		Jugador luis = new JugadorConcreto("Luis");
+		Turno turno = new Turno(juan, pepe, luis);		
+		Casilla carcel = new Carcel();
+		
+		carcel.afectar(pepe);
+		carcel.pagarFianza(pepe);
+		
+		Jugador jugadorSiguiente = turno.cambiarTurno();
+		Assert.assertEquals(jugadorSiguiente, luis);
+		
 	}
 	
 }
