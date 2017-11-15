78781aeb0c5cfbd70929ebee64126ff4270d2845
lopezlin <lopezlin@IBM123-PB01HC22.argentina.ibm.com>
2017-11-15 18:37:34 -0300

    PoliciaTest() y QuiniTest()

---
+++ test/unitarios/QuiniTest.java
@@ -0,0 +1,53 @@
+package unitarios;
+
+import org.junit.Assert;
+import org.junit.Test;
+import entidades.JugadorConcreto;
+import entidades.Jugador;
+import acciones.Quini;
+
+
+
+public class QuiniTest {
+	
+/***********************************************/	
+
+	@Test
+	public void test01CaenEnQuiniYCapitalSeIncrementaEn50000() {
+		
+		Jugador jugador = new JugadorConcreto ("Jose PEPE Argento");
+		Quini quini6 = new Quini ();
+		quini6.afectar(jugador);
+		Assert.assertEquals(150000,jugador.capital());
+		
+	}
+
+/***********************************************/
+	
+	@Test
+	public void test02CaenEnQuiniPorSegundaVezYCapitalSeIncrementaEn30000() {
+		
+		Jugador jugador = new JugadorConcreto ("Jose PEPE Argento");
+		Quini quini6 = new Quini ();
+		quini6.afectar(jugador);
+		quini6.afectar(jugador);
+		Assert.assertEquals(180000,jugador.capital());
+		
+	}
+	
+/***********************************************/
+	
+	@Test
+	public void test03CaenEnQuiniPorTerceraVezYCapitalNoSeIncrementa() {
+		
+		Jugador jugador = new JugadorConcreto ("Jose PEPE Argento");
+		Quini quini6 = new Quini ();
+		quini6.afectar(jugador);
+		quini6.afectar(jugador);
+		Assert.assertEquals(180000,jugador.capital());
+		
+	}
+	
+/***********************************************/
+	
+}
