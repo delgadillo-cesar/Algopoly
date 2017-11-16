0049e7dfaf95a75ce8e43898b09343ca7029257f
Mateo <mateoicalvo@users.noreply.github.com>
2017-11-15 21:14:26 -0300

    Agregados test de construccion

--- test/unitarios/ConstruccionTest.java
+++ test/unitarios/ConstruccionTest.java
@@ -38,4 +38,80 @@ public class ConstruccionTest {
 		
 		Assert.assertNotNull(terreno);
 	}
+	
+	@Test
+	public void testConstruirEdificioHistoricoNoDevuelveNull() {
+		
+		Construccion terreno = Construccion.construirUnEdificioHistorico(2500, 2500);
+		
+		Assert.assertNotNull(terreno);
+	}
+	
+	@Test
+	public void testObtenerPrecioAlquilerDevuelveCorrectamenteElCosto() {
+		
+		Construccion terreno = Construccion.construirUnTerreno(1500);
+		
+		int precioAlquiler = terreno.precioAlquiler();
+		
+		Assert.assertEquals(precioAlquiler, 1500);
+	}
+	
+	@Test
+	public void testObtenerDescripcionDevuelveCorrectamenteLaDescripcionParaUnTerreno() {		
+		
+		Construccion terreno = Construccion.construirUnTerreno(1500);
+		
+		String descripcion = terreno.descripcion();
+		
+		Assert.assertEquals(descripcion, "un terreno");
+		
+	}
+	
+	@Test
+	public void testObtenerDescripcionDevuelveCorrectamenteLaDescripcionParaUnaCasa() {		
+		Construccion terreno = Construccion.construirUnaCasa(1500, 1500);
+		
+		String descripcion = terreno.descripcion();
+		
+		Assert.assertEquals(descripcion, "una casa");
+	}
+	
+	@Test
+	public void testObtenerDescripcionDevuelveCorrectamenteLaDescripcionParaDosCasas() {		
+		Construccion terreno = Construccion.construirDosCasa(1500, 1500);
+		
+		String descripcion = terreno.descripcion();
+		
+		Assert.assertEquals(descripcion, "dos casas");
+	}
+	
+	@Test
+	public void testObtenerDescripcionDevuelveCorrectamenteLaDescripcionParaUnHotel() {		
+		
+		Construccion terreno = Construccion.construirUnHotel(1500, 1500);
+		
+		String descripcion = terreno.descripcion();
+		
+		Assert.assertEquals(descripcion, "un hotel");
+	}
+	
+	@Test
+	public void testObtenerDescripcionDevuelveCorrectamenteLaDescripcionParaUnEdificioHistorico() {		
+		Construccion terreno = Construccion.construirUnEdificioHistorico(1500, 1500);
+		
+		String descripcion = terreno.descripcion();
+		
+		Assert.assertEquals(descripcion, "un edificio historico");
+	}
+	
+	@Test
+	public void testObtenerCostoConstruccionDevuelveCorrectamenteElCostoDeLaConstruccion() {
+		
+		Construccion terreno = Construccion.construirUnHotel(1500, 1500);
+		
+		int costoConstruccion = terreno.costoConstruccion();
+		
+		Assert.assertEquals(costoConstruccion, 1500);
+	}
 }
