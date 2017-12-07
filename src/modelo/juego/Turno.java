e1710f1d26fc924960908471e851ce6cb7b48d12
Mateo <mateoicalvo@users.noreply.github.com>
2017-12-07 17:26:34 -0300

    Refactor Turno

--- src/modelo/juego/Turno.java
+++ src/modelo/juego/Turno.java
@@ -5,21 +5,21 @@ import java.util.LinkedList;
 import modelo.entidades.Jugador;
 
 public class Turno {
-
-	private LinkedList<Jugador> jugadores;
-
-	private Jugador jugadorActual;
-	private Jugador jugadorAnterior;
+	
 	private static Turno elTurno;
+	
+	
+	private LinkedList<Jugador> jugadores;
+	
+	private Jugador jugadorAnterior;
 
-	private Turno(LinkedList<Jugador> jugadores) {
-		this.jugadores = jugadores;
-		this.jugadorActual = this.jugadores.getFirst();
-		this.jugadorAnterior = this.jugadores.getLast();
+	private Turno(LinkedList<Jugador> jugadoresDelJuego) {
+		jugadores = jugadoresDelJuego;
+		jugadorAnterior = jugadoresDelJuego.getLast();
 	}
 
-	public static Turno inicializarTurno(LinkedList<Jugador> jugadores){
-		elTurno = new Turno(jugadores);
+	public static Turno inicializarTurno(LinkedList<Jugador> jugadoresDelJuego){
+		elTurno = new Turno(jugadoresDelJuego);
 		return elTurno;
 	}
 
@@ -31,40 +31,22 @@ public class Turno {
 	}
 	
 	public Jugador turnoActual(){
-		return this.jugadorActual;
+		return jugadores.peek();
 	}
 	
 	public Jugador cambiarTurno() {
-		if (Dados.getInstance().salieronPares() && !this.jugadorActual.equals(this.jugadorAnterior)) { // Dados pares &&
-			this.repetirTurno();
-		} else {
-			this.avanzarTurno();
+		if (Dados.getInstance().salieronPares() && (jugadores.peek() != jugadorAnterior)) {
+			jugadorAnterior = jugadores.peek();
+			return jugadores.peek();
 		}
-/*
-		if (!Dados.getInstance().salieronPares()) {
-			Carcel laCarcel = (Carcel) Tablero.getInstance().obtenerCasilla(6);
-			laCarcel.revizarCondena(jugadorActual);
-		}
-*/
-		return this.jugadorActual;
-	}
-
-	private void repetirTurno() {
-		this.jugadorAnterior = this.jugadorActual;
-	}
-
-	private void avanzarTurno() {
-		this.repetirTurno();
-		this.jugadores.removeFirst();
-		this.jugadores.addLast(jugadorAnterior);
-		this.jugadorActual = this.jugadores.getFirst();
-	}
+		Jugador actual = jugadores.poll();
+		jugadores.add(actual);
+		return jugadores.peek();
+	}	
 
 	public void quitarJugadorActual() {
-		jugadores.remove(this.jugadorActual);
-		this.jugadorActual = this.jugadores.getFirst();
-		this.jugadorAnterior = this.jugadores.getLast();
-		this.jugadores.removeLast();
+		
+		jugadores.poll();
 	}
 
 }
