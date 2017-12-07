af57806e4611abe35cc05ca78c1e229f7531fa3c
hpavlo <hpavlo@hpavlo>
2017-12-07 12:48:02 -0300

    Se Creo la clase de Test para Barrios (generica)

--- src/modelo/tablero/poseibles/barrios/Barrio.java
+++ src/modelo/tablero/poseibles/barrios/Barrio.java
@@ -9,13 +9,12 @@ import modelo.tablero.poseibles.barrios.construcciones.NadaParaConstruirExceptio
 import modelo.tablero.poseibles.barrios.construcciones.TerrenoConstruible;
 import modelo.tablero.poseibles.compania.*;
 
+public abstract class Barrio implements Poseible {
 
-public abstract class Barrio implements Poseible  {
-	
 	protected Jugador propietario;
 	protected Construccion construido;
 	protected ConstruccionPosible construcciones;
-	
+
 	public Barrio(Jugador propietario) {
 		TerrenoConstruible terreno = new TerrenoConstruible();
 
@@ -23,53 +22,75 @@ public abstract class Barrio implements Poseible  {
 		this.construido = terreno.construirEn(this);
 	}
 
-	public Jugador propietario(){
+	public Jugador propietario() {
 		return this.propietario;
 	}
-	
-	public int cantidadDePropiedades(){
+
+	public int cantidadDePropiedades() {
 		return (this.construido.cantidadDePropiedades());
 	}
-	
-	public void construir() throws NadaParaConstruirException{
-		Construccion nuevaConstruccion = this.construcciones.construir(this);
-		this.construido = nuevaConstruccion;
+
+	public void construir() throws NadaParaConstruirException {
+		this.construido = this.construcciones.construir(this);
 	}
-	
-	public int costoConstruccion() throws NadaParaConstruirException{
+
+	public int costoConstruccion() throws NadaParaConstruirException {
 		return this.construcciones.costoConstruccion(this);
 	}
-	
-	public Construccion construccionActual(){
+
+	public Construccion construccionActual() {
 		return this.construido;
 	}
-	
+
 	public void afectar(Jugador jugador) throws MismoJugadorException {
 		this.construido.cobrarAlquiler(this.propietario, jugador);
 	}
-	
-	public boolean sePuedeVenderConstruccion(){
+
+	public boolean sePuedeVenderConstruccion() {
 		return this.construido.sePuedeVender();
 	}
-	
-	public void venderConstruccion(){
+
+	public void venderConstruccion() {
 		this.construido = this.construido.vender();
 	}
 
 	public abstract void valoresDeTerreno(Construible unConstruible);
+
 	public abstract void valoresDeUnaCasa(Construible unConstruible);
+
 	public abstract void valoresDeDosCasas(Construible unConstruible);
+
 	public abstract void valoresDeHotel(Construible unConstruible);
+
 	public abstract void valoresDeEdificioHistorico(Construible unConstruible);
-	
-	public void interactuarCon(Barrio otroBarrio){}
-	public void interactuarConBuenosAiresNorte(Barrio otroBarrio){}
-	public void interactuarConBuenosAiresSur(Barrio otroBarrio){}
-	public void interactuarConCordobaNorte(Barrio otroBarrio){}
-	public void interactuarConCordobaSur(Barrio otroBarrio){}
-	public void interactuarConSaltaNorte(Barrio otroBarrio){}
-	public void interactuarConSaltaSur(Barrio otroBarrio){}
-	public void interactuarConNeuquen(Barrio otroBarrio){}
-	public void interactuarConSantaFe(Barrio otroBarrio){}
-	public void interactuarConTucuman(Barrio otroBarrio){}
+
+	public void interactuarCon(Barrio otroBarrio) {
+	}
+
+	public void interactuarConBuenosAiresNorte(Barrio otroBarrio) {
+	}
+
+	public void interactuarConBuenosAiresSur(Barrio otroBarrio) {
+	}
+
+	public void interactuarConCordobaNorte(Barrio otroBarrio) {
+	}
+
+	public void interactuarConCordobaSur(Barrio otroBarrio) {
+	}
+
+	public void interactuarConSaltaNorte(Barrio otroBarrio) {
+	}
+
+	public void interactuarConSaltaSur(Barrio otroBarrio) {
+	}
+
+	public void interactuarConNeuquen(Barrio otroBarrio) {
+	}
+
+	public void interactuarConSantaFe(Barrio otroBarrio) {
+	}
+
+	public void interactuarConTucuman(Barrio otroBarrio) {
+	}
 }
